9f10afc9203f299e97561ba04d9bae5dd0f12688
Jonathan Dunlap <jdunlap@outlook.com>
2020-06-25 17:09:26 -0700

    init

---
+++ src/pages/index.svelte
@@ -0,0 +1,50 @@
+<style>
+  h1,
+  figure,
+  p {
+    text-align: center;
+    margin: 0 auto;
+  }
+
+  h2 {
+    font-size: 2.8em;
+    text-transform: uppercase;
+    font-weight: 700;
+    margin: 0 0 0.5em 0;
+  }
+
+  figure {
+    margin: 0 0 1em 0;
+  }
+
+  p {
+    margin: 1em auto;
+  }
+
+  @media (min-width: 480px) {
+    h1 {
+      font-size: 4em;
+    }
+  }
+</style>
+
+<script>
+
+</script>
+
+<svelte:head>
+  <title>Sapper project template</title>
+</svelte:head>
+
+<h1 class="text-gray-800 uppercase font-bold mb-4">Great success!</h1>
+
+<figure class="items-center w-full">
+  <img class="max-w-xl m-auto w-4/5" alt="Success Kid" src="//placekitten.com/200/300" />
+  <figcaption>Have fun with Sapper!</figcaption>
+</figure>
+
+<p>
+  <strong>
+    Try editing this file (src/routes/index.svelte) to test live reloading.
+  </strong>
+</p>
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    purge: {
+      enabled: true,
+      content: ["./src/**/*.html", "./src/**/*.svelte"],
+    },
+    theme: {
+      extend: {},
+    },
+    variants: {},
+    plugins: [],
+  };
+  
