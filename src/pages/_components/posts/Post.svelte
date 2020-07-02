387abca7d3c8859288a2228326e78bff4660247b
Jonathan Dunlap <jdunlap@outlook.com>
2020-07-02 11:59:29 -0700

    improve cards

--- src/pages/_components/posts/Post.svelte
+++ src/pages/_components/posts/Post.svelte
@@ -1,14 +1,14 @@
 <script>
 </script>
 
-<figure class="mdc-card demo-card w-full md:max-w-3x1 float-inital">
+<figure class="mdc-card demo-card w-full md:max-w-3x1 sm:w-full my-4 md:max-w-4xl">
   <div class="mdc-card__primary-action demo-card__primary-action" tabindex="0">
-    <div class="mdc-card__media mdc-card__media--16-9 demo-card__media" style="background-image: url(&quot;https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/2.jpg&quot;);"></div>
+    <div class="mdc-card__media mdc-card__media--16-9 demo-card__media sm:w-full" style="background-image: url(&quot;https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/2.jpg&quot;);"></div>
     <div class="demo-card__primary">
       <h2 class="demo-card__title mdc-typography mdc-typography--headline6">Our Changing Planet</h2>
       <h3 class="demo-card__subtitle mdc-typography mdc-typography--subtitle2">by Kurt Wagner</h3>
     </div>
-    <div class="demo-card__secondary mdc-typography mdc-typography--body2 p-4">Visit ten places on our planet that are undergoing the biggest changes today.</div>
+    <div class="demo-card__secondary mdc-typography mdc-typography--body2">Visit ten places on our planet that are undergoing the biggest changes today.</div>
   </div>
   <div class="mdc-card__actions">
     <div class="mdc-card__action-buttons">
--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -14,20 +14,20 @@ metatags.title = "home";
 const data = [{ title: 'ASMR preview video', id: '123', cost: ''}];
 </script>
 
-<h1 class="ml text-gray-800 uppercase font-bold mb-4 mx-auto text-center">Sugar Boogerz</h1>
+<h1 class="ml text-white uppercase font-bold mb-4 mx-auto text-center">Sugar Boogerz</h1>
 
 
-<section class="text-gray-700 body-font m-4">
+<section class="text-gray-700 body-font m-0">
   <div class="container px-5 sm:px-0 py-24 mx-auto">
-    <div class="flex flex-wrap -m-4 justify-center items-center">
+    <div class="flex flex-wrap m-4 justify-center items-center">
     {#each [1,2,3] as index}
-      <div class="flex-initial m-5" 
+      <div class="flex-initial md:m-5 sm:w-full md:w-auto" 
         in:fly="{{ y: 200, duration: index*300, delay: index*300 }}" >
         <Post/>
       </div>
      {/each}
   
-  <div class="py-2 fixed right-0 bottom-0 m-8">
+  <div class="fixed right-0 bottom-0 md:m-4">
     <button class="mdc-fab" aria-label="Favorite" on:click="{()=>$goto('/upload')}" >
       <div class="mdc-fab__ripple"></div>
       <span class="mdc-fab__icon material-icons">publish</span>
