354564c1348725d86f317d7b3e1a1838b461fadb
Jonathan Dunlap <jdunlap@outlook.com>
2020-07-03 10:49:14 -0700

    cleanup configs

---
+++ pages/_document.js
@@ -0,0 +1,23 @@
+import Document, { Head, Main, NextScript } from 'next/document';
+import React from 'react';
+
+class CustomDocument extends Document {
+  render() {
+    return (
+      <html lang="en">
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
+
+export default CustomDocument;
