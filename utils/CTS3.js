d31104ae741bf93bee0fc645034f7519bc3c5104
Jonathan Dunlap <jdunlap@outlook.com>
2020-07-08 13:09:05 -0700

    upload working

--- utils/CTS3.js
+++ utils/CTS3.js
@@ -1,22 +1,41 @@
-import AWS from 'aws-sdk';
+const AWS = require('aws-sdk/global');
+const S3 = require('aws-sdk/clients/s3');
+AWS.S3 = S3;
+// import { AmazonCognitoIdentity } from 'amazon-cognito-identity-js';
 
-var albumBucketName = "collabtube-public";
-var bucketRegion = "us-east-2";
-var IdentityPoolId = "us-east-2:ed15ab27-205e-4582-91c7-56bf29d7bbb3";
+const albumBucketName = "collabtube-public";
+const bucketRegion = "us-east-2";
+const IdentityPoolId = "us-east-2:ed15ab27-205e-4582-91c7-56bf29d7bbb3";
 
-AWS.config.update({
-  region: bucketRegion,
-  credentials: new AWS.CognitoIdentityCredentials({
+// AWS.Cognito.config.update({accessKeyId: 'anything', secretAccessKey: 'anything'});
+
+let init = false;
+function setup() {
+  if(init) return;
+  init = true;
+
+  const cred = new AWS.CognitoIdentityCredentials({
     IdentityPoolId: IdentityPoolId
-  })
-});
+  });
+  AWS.config.credential = cred;
+  AWS.config.update({
+    region: bucketRegion,
+    credentials: cred
+  });
 
-var s3 = new AWS.S3({
-  apiVersion: "2006-03-01",
-  params: { Bucket: albumBucketName }
-});
+  AWS.config.getCredentials(function (err) {
+    if (err) console.log(err); // credentials not loaded
+    else console.log("Access Key:", AWS.config.credentials.accessKeyId);
+  })
+}
 
 export function addVideo(files) {
+    setup();
+    var s3 = new AWS.S3({
+      apiVersion: "2006-03-01",
+      params: { Bucket: albumBucketName }
+    });
+
     const albumName = '';
     // var files = document.getElementById("photoupload").files;
     if (!files.length) {
@@ -26,15 +45,15 @@ export function addVideo(files) {
     var fileName = file.name;
     var albumPhotosKey = encodeURIComponent(albumName) + "/";
   
-    var photoKey = albumPhotosKey + fileName;
+    var photoKey = fileName; // albumPhotosKey + 
   
     // Use S3 ManagedUpload class as it supports multipart uploads
     var upload = new AWS.S3.ManagedUpload({
       params: {
         Bucket: albumBucketName,
         Key: photoKey,
-        Body: file,
-        ACL: "public-read"
+        Body: file
+        // ACL: "public-read"
       }
     });
   
@@ -46,6 +65,7 @@ export function addVideo(files) {
         // viewAlbum(albumName);
       },
       function(err) {
+        console.log('err', err);
         return alert("There was an error uploading your photo: ", err.message);
       }
     );
