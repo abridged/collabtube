4ae34149683bb1aaba3baa2fd358d25f86dcbf61
Jonathan Dunlap <jdunlap@outlook.com>
2020-07-25 23:38:46 -0700

    tests

---
+++ pages/test.js
@@ -0,0 +1,38 @@
+import Page from "../components/Page";
+import { useEffect, useState } from "react";
+const Box = require("3box");
+const { Feed } = require("../utils/Feed");
+
+export default function Index() {
+  const [profile, setProfile] = useState(null);
+  const [box, setBox] = useState(null);
+
+  async function getProfile() {
+    if (window.ethereum.enable) await window.ethereum.enable();
+    else await window.ethereum.request({ method: "eth_requestAccounts" });
+
+    const addr = await window.ethereum.request({ method: "eth_accounts" });
+    console.log("addr", addr);
+    const box = await Box.openBox(addr[0], window.ethereum);
+    setBox(box);
+    console.log("box finish");
+    const _profile = await Box.getProfile(
+      "0xffaDc07f1BFb127F4312e8652fE94aB0c771b54D"
+    );
+    console.log(_profile);
+    setProfile(_profile);
+    console.log("_profile finish");
+
+    const f = new Feed(box);
+    await f.init();
+    console.log("init finish");
+    await f.save();
+    console.log("finish");
+  }
+
+  useEffect(() => {
+    getProfile();
+  }, []);
+
+  return <div>This is a test</div>;
+}
--- utils/Feed.js
+++ utils/Feed.js
@@ -4,21 +4,34 @@ export function test() {
   return { test: "hello" };
 }
 
+const SPACE_APP = "fame";
+
 export function getNewVideos() {
-  const s = Box.getSpace("0xffaDc07f1BFb127F4312e8652fE94aB0c771b54D", "fame");
+  const s = Box.getSpace(
+    "0xffaDc07f1BFb127F4312e8652fE94aB0c771b54D",
+    SPACE_APP
+  );
 
   return s.public.all().videos || [];
 }
 
-const SPACE_APP = "fame";
+// const box = await Box.create(window.ethereum);
 
 export class Feed {
-  constructor() {
-    this.box = await Box.create(window.ethereum);
-    this.space = await this.box.openSpace(SPACE_APP);
+  constructor(box) {
+    this.box = box;
   }
 
-  test() {
-      this.space.public.set('hello', 'world');
+  async init() {
+    this.box.openSpace(SPACE_APP);
+    this.space = await this.box.openSpace(SPACE_APP);
+    return this.space;
+  }
+
+  async save() {
+    await this.space.public.set("hello", "world");
+
+    const d = await this.space.public.get("hello");
+    console.log("data get:", d);
   }
 }
