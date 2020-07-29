20e7b419baa8deef16f4f10fd712ea1db6a10787
jennifertrin <38402540+jennifertrin@users.noreply.github.com>
2020-07-28 21:22:04 -0400

    Revert "started currency converter"
    This reverts commit 17cdbf131296e707898dd22cc662ca86b4bde969.

--- pages/setTicket.js
+++ pages/setTicket.js
@@ -2,8 +2,6 @@ import React, { useState } from "react";
 import { addVideo } from "../utils/CTS3";
 import { TextField, Slider, Typography, Button } from "@material-ui/core";
 import { Icon, InlineIcon } from '@iconify/react';
-import data from "../data/data"
-import Converter from "../components/tokenConverter2"
 
 import LoadingOverlay from '../components/LoadingOverlay';
 
@@ -11,8 +9,6 @@ function setTicket() {
 
     return (
         <div>
-
-
             <div class="flex justify-center">
                 <hr class="bg-gray-400 h-1 border-transparent w-1/2 m-8"></hr>
             </div>
@@ -34,9 +30,31 @@ function setTicket() {
             </div>
 
             <div class="mt-10 flex justify-center">
-                <Converter />
+                <button class="justify-center my-1 bg-white hover:bg-gray-400 text-black font-semibold w-16 py-2 px-4 border-2 border-gray-400 rounded shadow m-1">
+                    <div class="flex justify-center">
+                        <span class="justify-center align-middle">MYS</span>
+                    </div>
+                </button>
+                <input class="my-1 bg-white hover:bg-gray-100 text-black font-semibold w-1/6 py-2 px-4 border-2 border-gray-400 rounded shadow m-1" />
             </div>
 
+            <div class="flex justify-center">
+                <p class="justify-end text-gray-500 text-sm">Token Price: $0.50</p>
+            </div>
+
+            <div class="mt-10 flex justify-center">
+                <button class="justify-center align-middle my-1 bg-white hover:bg-gray-400 text-black font-semibold w-16 py-2 px-4 border-2 border-gray-400 rounded shadow m-1">
+                    <div class="flex justify-center">
+                        <svg class="h-6 w-10" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" preserveAspectRatio="xMidYMid meet" viewBox="0 0 32 32">
+                            <path d="M15 3v2.094C12.164 5.57 10 8.05 10 11c0 1.777.832 3.148 1.938 4.031c1.105.883 2.437 1.375 3.687 1.875s2.418 1.008 3.188 1.625C19.581 19.148 20 19.773 20 21c0 2.156-1.793 4-4 4c-2.219 0-4-1.781-4-4v-1h-2v1c0 2.965 2.164 5.43 5 5.906V29h2v-2.094c2.844-.48 5-2.98 5-5.906c0-1.781-.832-3.145-1.938-4.031c-1.105-.887-2.437-1.407-3.687-1.907s-2.418-.98-3.188-1.593C12.418 12.855 12 12.219 12 11c0-2.191 1.785-4 4-4c2.219 0 4 1.781 4 4v1h2v-1c0-2.965-2.164-5.43-5-5.906V3z" fill="black" /></svg>
+                        <span class="justify-center align-middle">USD</span>
+                    </div>
+                </button>
+                <input class="my-1 bg-white hover:bg-gray-100 text-black font-semibold w-1/6 py-2 px-4 border-2 border-gray-400 rounded shadow m-1" />
+            </div>
+            <div class="flex justify-center">
+                <p class="justify-end text-gray-500 text-sm">Account Balance: $34.00</p>
+            </div>
 
             {/*Checkmark icon*/}
             <div class="flex justify-center">
@@ -45,6 +63,8 @@ function setTicket() {
                 </button>
             </div>
         </div >
+
+
     )
 }
 
