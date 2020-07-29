ccdbf74b033ddccb5d8876e46608114aa64cc616
jennifertrin <38402540+jennifertrin@users.noreply.github.com>
2020-07-29 08:47:10 -0400

    added currency converter

---
+++ data/Data.js
@@ -0,0 +1,19 @@
+const data = {
+    "currencies": [
+        {
+            "code": "MSK",
+            "sellRate": 1,
+            "name": "MSK Token",
+        },
+    ],
+    "fiat": [
+        {
+            "code": "USD",
+            "sellRate": .7041,
+            "name": "United States Dollars",
+        }
+    ]
+
+}
+
+export default data;
