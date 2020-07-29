17cdbf131296e707898dd22cc662ca86b4bde969
jennifertrin <38402540+jennifertrin@users.noreply.github.com>
2020-07-28 21:08:15 -0400

    started currency converter

---
+++ data/data.js
@@ -0,0 +1,16 @@
+const data = {
+    "currencies": [
+        {
+            "code": "MKS",
+            "sellRate": 1,
+            "name": "MKS Token",
+        },
+        {
+            "code": "USD",
+            "sellRate": .7041,
+            "name": "United States Dollars",
+        }
+    ]
+}
+
+export default data;
